37cc9e02365949f7a1150cdcac9f66cb97cbc950
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-12-10 15:27:22 +0100

    fixed request tokens for node

--- node/tokens/request.js
+++ node/tokens/request.js
@@ -4,13 +4,9 @@ const input = require('../input');
 
 (async () => {
     const ocean = await Ocean.getInstance(config)
-
     const accounts = await ocean.getAccounts()
-    Logger.log(accounts)
-    const account = accounts.filter(account => account.id === config.address)[0]
-    Logger.log(account, input)
-    await account.requestToken(input)
-    // BUG: doesn't update balance
+    const account = accounts.filter(account => account.getId().toLowerCase() === config.address)[0]
+    await account.requestTokens(input)
     const balance = await account.getOceanBalance()
     Logger.log(balance)
     Logger.log(`__result__${account.id} received ${input} tokens`)
